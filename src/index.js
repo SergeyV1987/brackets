923d8f8bac75f58372cc3bf1c1110667c277d5bc
Admin <kurchinskayavera@gmail.com>
2019-10-10 22:40:59 +0300

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = {};
+    let openingBrackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+
+    for (let c of str) {
+        if (brackets[openingBrackets[openingBrackets.length - 1]] === c) {
+            openingBrackets.pop();
+        } else if (c in brackets) {
+            openingBrackets.push(c);
+        } else {
+            return false;
+        }
+    }
+
+    return openingBrackets.length === 0;
+};
